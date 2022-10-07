15475a48f2c694f9991c389b2450fa5155425a38
Vik_toria <72942985+viktoria1108@users.noreply.github.com>
2022-10-07 21:26:07 +0300

    gamesolution

---
+++ Game2/Assets/TrigerMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrigerMovement : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Door;
+    bool isOpened = false;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (!isOpened)
+        {
+            isOpened = true;
+            Door.transform.position += new Vector3(-4, 0, 0);
+
+        }
+
+    }
+}
